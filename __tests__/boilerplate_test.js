2af2ddb8db2deb37cdf16d4bcdacdd2591ddeda8
Paulo Cesar Moura Nunes Americo Coutinho <paulo.coutinho@serpro.gov.br>
2017-10-17 15:40:20 -0300

    dependencies bugfixes

--- __tests__/boilerplate_test.js
+++ __tests__/boilerplate_test.js
@@ -6,10 +6,15 @@
 //   Simulate
 // } from 'react-addons-test-utils';
 import { expect } from 'chai';
+import React from 'react';
+import Wizard from '../src';
 
 describe('Boilerplate', function () {
   it('should do boilerplate things', function () {
     // TODO: test something now
+
+    let wiz = <Wizard />;
+
     expect(true).to.equal(true);
   });
 });
